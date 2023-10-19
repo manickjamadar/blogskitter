78a34136b00f59b1c716ffbf3f106da3e3b807c4
Manick <manickware@gmail.com>
2023-10-19 17:12:26 +0530

    auth form implemented in header component

--- src/views/header/header.tsx
+++ src/views/header/header.tsx
@@ -1,18 +1,54 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Modal from "../modal/modal";
+import AuthForm, { AuthFormData } from "../auth_form/auth_form";
 
 const Header = () => {
+  // Auth Modal State => 0 for close, 1 for Signin, 2 for Signup
+  const [authModalState, setAuthModalState] = useState(0);
+  const openSigninModal = () => {
+    setAuthModalState(1);
+  };
+  const openSignupModal = () => {
+    setAuthModalState(2);
+  };
+  const closeAuthModal = () => {
+    setAuthModalState(0);
+  };
+  const handleAuthSubmit = (data: AuthFormData, isSignup: boolean) => {
+    console.log("Auth Form Data: ", data);
+  };
+  const isModalOpen = authModalState !== 0;
+  //   const isSigninModalOpen = authModalState === 1;
+  const isSignupModalOpen = authModalState === 2;
   return (
-    <div className="flex justify-between p-4 border-b-2 items-center">
-      <h1 className="text-xl">Blogskitter</h1>
-      <div className="flex gap-6">
-        <button className="border border-blue-500 px-4 py-2 rounded">
-          Sign In
-        </button>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">
-          Sign Up
-        </button>
+    <>
+      <div className="flex justify-between p-4 border-b-2 items-center">
+        <h1 className="text-xl">Blogskitter Manick lala</h1>
+        <div className="flex gap-6">
+          <button
+            className="border border-blue-500 px-4 py-2 rounded"
+            onClick={openSigninModal}
+          >
+            Sign In
+          </button>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+            onClick={openSignupModal}
+          >
+            Sign Up
+          </button>
+        </div>
       </div>
-    </div>
+      <Modal isOpen={isModalOpen} onClose={closeAuthModal}>
+        <AuthForm
+          isSignup={isSignupModalOpen}
+          onSigninClick={openSigninModal}
+          onSignupClick={openSignupModal}
+          onSubmit={handleAuthSubmit}
+        />
+      </Modal>
+    </>
   );
 };
 
