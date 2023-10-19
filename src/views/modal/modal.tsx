27171a6ebf698eb625377dbb367b29d8d7533e94
Manick <manickware@gmail.com>
2023-10-19 17:08:22 +0530

    padding added in modal component

--- src/views/modal/modal.tsx
+++ src/views/modal/modal.tsx
@@ -12,11 +12,13 @@ const Modal: React.FC<Props> = ({ isOpen, onClose, children }) => {
           className="fixed z-10 top-0 left-0 w-screen h-screen bg-black bg-opacity-70 flex justify-center items-center"
           onClick={onClose}
         >
-          <div
-            className="bg-white rounded"
-            onClick={(e) => e.stopPropagation()}
-          >
-            {children}
+          <div className="w-full max-w-md p-6">
+            <div
+              className="bg-white rounded w-full"
+              onClick={(e) => e.stopPropagation()}
+            >
+              {children}
+            </div>
           </div>
         </div>
       )}
