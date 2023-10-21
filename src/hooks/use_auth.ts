ffa20c8133ea23c72fc942427c6750fe9181dd62
Manick <manickware@gmail.com>
2023-10-21 09:20:13 +0530

    login and logout method removed from use auth hook

--- src/hooks/use_auth.ts
+++ src/hooks/use_auth.ts
@@ -1,32 +1,20 @@
 import UserModel from "@/domain/models/user";
 import AuthSelector from "@/store/selectors/auth_selector";
-import { AuthActions } from "@/store/slices/auth_slice";
-import { useAppDispatch, useAppSelector } from "@/store/store";
+import { useAppSelector } from "@/store/store";
 
 interface UseAuthReturnType {
   isLoggedIn: boolean;
   isLoggedOut: boolean;
   user?: UserModel;
-  logout: () => void;
-  login: (user: UserModel) => void;
 }
 const useAuth = (): UseAuthReturnType => {
-  const dispatch = useAppDispatch();
   const isLoggedIn = useAppSelector(AuthSelector.isLoggedIn);
   const isLoggedOut = useAppSelector(AuthSelector.isLoggedOut);
   const user = useAppSelector(AuthSelector.user);
-  const logout = () => {
-    dispatch(AuthActions.logout());
-  };
-  const login = (user: UserModel) => {
-    dispatch(AuthActions.login({ user }));
-  };
   return {
     isLoggedIn,
     isLoggedOut,
     user,
-    logout,
-    login,
   };
 };
 export default useAuth;
