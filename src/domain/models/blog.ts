83a4bd05726f34539f73cc5c19292bdeab1ecedd
Manick <manickware@gmail.com>
2023-10-22 17:33:34 +0530

    blog model class created

---
+++ src/domain/models/blog.ts
@@ -0,0 +1,62 @@
+import { Timestamp } from "firebase-admin/firestore";
+
+export interface IBlogModel {
+  id: string;
+  title: string;
+  categories: string[];
+  description: string;
+  uid: string;
+  createdDate: string;
+  coverImageUrl: string;
+}
+interface BlogDocument {
+  id: string;
+  title: string;
+  categories: string[];
+  description: string;
+  uid: string;
+  createdDate: Timestamp;
+  coverImageUrl: string;
+}
+class BlogModel implements IBlogModel {
+  public id: string;
+  public title: string;
+  public categories: string[];
+  public description: string;
+  public uid: string;
+  public createdDate: string;
+  public coverImageUrl: string;
+  constructor(data: IBlogModel) {
+    this.id = data.id;
+    this.title = data.title;
+    this.categories = [...data.categories];
+    this.description = data.description;
+    this.uid = data.uid;
+    this.createdDate = data.createdDate;
+    this.coverImageUrl = data.coverImageUrl;
+  }
+  toJson(): IBlogModel {
+    return {
+      id: this.id,
+      title: this.title,
+      categories: [...this.categories],
+      coverImageUrl: this.coverImageUrl,
+      uid: this.uid,
+      description: this.description,
+      createdDate: this.createdDate,
+    };
+  }
+  toFirebase(): BlogDocument {
+    return {
+      id: this.id,
+      title: this.title,
+      categories: [...this.categories],
+      coverImageUrl: this.coverImageUrl,
+      uid: this.uid,
+      description: this.description,
+      createdDate: Timestamp.fromDate(new Date(this.createdDate)),
+    };
+  }
+}
+
+export default BlogModel;
