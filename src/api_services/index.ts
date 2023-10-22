7701e4d5b5f4a28e57429ab31b4337422356cab3
Manick <manickware@gmail.com>
2023-10-22 17:37:50 +0530

    api services exported

---
+++ src/api_services/index.ts
@@ -0,0 +1,6 @@
+import FirebaseApiAuthService from "./api_auth_service/firebase_api_auth_service";
+import FirebaseApiBlogService from "./api_blog_service/firebase_api_blog_service";
+
+const firebaseApiAuthService = new FirebaseApiAuthService();
+const firebaseApiBlogService = new FirebaseApiBlogService();
+export { firebaseApiAuthService, firebaseApiBlogService };
