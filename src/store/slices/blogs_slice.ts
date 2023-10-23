f118096b780e23408f12fcadc6fb58c7dd3fb23d
Manick <manickware@gmail.com>
2023-10-23 20:55:45 +0530

    real blog service added in blogs slice

--- src/store/slices/blogs_slice.ts
+++ src/store/slices/blogs_slice.ts
@@ -1,23 +1,24 @@
-import { getDemoBlogData } from "@/domain/data/demo_blog_data";
 import { IBlogModel } from "@/domain/models/blog";
+import { blogService } from "@/services";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 const sliceName = "blogs";
 const fetchBlogs = createAsyncThunk<
   { blogs: IBlogModel[]; canFetchMore: boolean; isInitialFetching: boolean },
-  { limit: number; skip: number; isInitialFetching?: boolean }
+  { limit: number; skip: number; isInitialFetching?: boolean },
+  { rejectValue: string }
 >(
   `${sliceName}/fetchBlogs`,
-  async ({ limit, skip, isInitialFetching = false }) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const blogs = getDemoBlogData(limit, skip);
-        resolve({
-          blogs,
-          canFetchMore: blogs.length === limit,
-          isInitialFetching,
-        });
-      }, 3000);
-    });
+  async ({ limit, skip, isInitialFetching = false }, { rejectWithValue }) => {
+    const blogsOrError = await blogService.fetchBlogs(limit, skip);
+    if (blogsOrError instanceof Error) {
+      return rejectWithValue(blogsOrError.message);
+    } else {
+      return {
+        blogs: blogsOrError,
+        canFetchMore: blogsOrError.length === limit,
+        isInitialFetching,
+      };
+    }
   }
 );
 interface BlogsState {
@@ -43,6 +44,7 @@ const blogsSlice = createSlice({
     builder.addCase(fetchBlogs.rejected, (state, action) => {
       state.errorMessage = "Blogs fetching failed";
       state.state = "error";
+      state.errorMessage = action.payload;
     });
     builder.addCase(fetchBlogs.fulfilled, (state, action) => {
       state.errorMessage = "";
