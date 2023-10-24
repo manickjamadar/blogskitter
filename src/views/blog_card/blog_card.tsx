a4875e39ea0fc22cb7ae6ccb2ad70bfa7eb419dc
Manick <manickware@gmail.com>
2023-10-24 19:50:33 +0530

    custom image used in blog card

--- src/views/blog_card/blog_card.tsx
+++ src/views/blog_card/blog_card.tsx
@@ -3,34 +3,24 @@ import config from "@/domain/config";
 import { IBlogModel } from "@/domain/models/blog";
 import formatDate from "@/utils/format_date";
 import truncateString from "@/utils/truncate_string";
-import Image from "next/image";
 import React, { useState } from "react";
+import CustomImage from "../custom_image/custom_image";
 interface Props {
   blog: IBlogModel;
   onClick?: () => void;
 }
 const BlogCard: React.FC<Props> = ({ blog, onClick }) => {
   const errorImageUrl = "/images/blog-cover-error-placeholder.png";
-  const [coverImageUrl, setCoverImageUrl] = useState(
-    blog.coverImageUrl ? blog.coverImageUrl : errorImageUrl
-  );
-  const placeholderDataUrl =
-    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/OXbHwAJmgPbgNt2+QAAAABJRU5ErkJggg==";
   return (
     <div
       onClick={onClick}
       className="cursor-pointer flex flex-col rounded-lg overflow-hidden w-full sm:w-[calc(50%-1.5rem)] lg:w-[calc(33%-1.5rem)] max-w-xs shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px]"
     >
-      <div className="bg-gray-100 aspect-video w-full relative">
-        <Image
-          src={coverImageUrl}
-          alt="blog cover placeholder image"
-          fill
-          className="object-cover"
-          placeholder={placeholderDataUrl}
-          onError={() => setCoverImageUrl(errorImageUrl)}
-        />
-      </div>
+      <CustomImage
+        src={blog.coverImageUrl}
+        alt="Blog Cover Image"
+        errorSrc={errorImageUrl}
+      />
       <div className="flex-1 flex flex-col py-4 px-4 gap-2">
         <p className="text-xs border px-2 py-[2px] rounded-full text-orange-400 self-start">
           {formatDate(new Date(blog.createdDate))}
