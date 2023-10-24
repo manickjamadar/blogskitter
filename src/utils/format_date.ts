5bd9c0aeafd31008ac99d290451ed3c2edb9934b
Manick <manickware@gmail.com>
2023-10-24 15:17:07 +0530

    format date function added

---
+++ src/utils/format_date.ts
@@ -0,0 +1,33 @@
+function formatDate(date: Date) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const suffixes = ["th", "st", "nd", "rd"];
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
+  const year = date.getFullYear();
+
+  let suffix = "th";
+  if (day % 10 === 1 && day !== 11) {
+    suffix = "st";
+  } else if (day % 10 === 2 && day !== 12) {
+    suffix = "nd";
+  } else if (day % 10 === 3 && day !== 13) {
+    suffix = "rd";
+  }
+
+  return `${day}${suffix} ${months[monthIndex]} ${year}`;
+}
+export default formatDate;
