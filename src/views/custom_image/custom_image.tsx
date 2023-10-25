b2bfa0dd9ba164f80c164703e0176adb668d2d3d
Manick <manickware@gmail.com>
2023-10-25 18:55:27 +0530

    raw image used in custom image

--- src/views/custom_image/custom_image.tsx
+++ src/views/custom_image/custom_image.tsx
@@ -1,25 +1,14 @@
-"use client";
-import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
+import RawImage from "../raw_image/raw_image";
 interface Props {
   alt: string;
   src: string;
   errorSrc: string;
 }
 const CustomImage: React.FC<Props> = ({ alt, src, errorSrc }) => {
-  const [actualSrc, setActualSrc] = useState(src ? src : errorSrc);
-  const placeholderDataUrl =
-    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/OXbHwAJmgPbgNt2+QAAAABJRU5ErkJggg==";
   return (
     <div className="bg-gray-100 aspect-video w-full relative">
-      <Image
-        src={actualSrc}
-        alt={alt}
-        fill
-        className="object-cover"
-        placeholder={placeholderDataUrl}
-        onError={() => setActualSrc(errorSrc)}
-      />
+      <RawImage alt={alt} src={src} errorSrc={errorSrc} />
     </div>
   );
 };
