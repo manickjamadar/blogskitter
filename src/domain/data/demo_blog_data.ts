20f85099dd502685fe8928836074948838cd5fba
Manick <manickware@gmail.com>
2023-10-25 18:54:16 +0530

    firebase image url added in demo blog data

--- src/domain/data/demo_blog_data.ts
+++ src/domain/data/demo_blog_data.ts
@@ -2,7 +2,8 @@ import { IBlogModel } from "../models/blog";
 const demoBlogData: IBlogModel[] = Array.from({ length: 10 }, (_, index) => ({
   id: Math.random().toString(36).substring(2, 7),
   categories: ["coding"],
-  coverImageUrl: "https://asdf.google.com",
+  coverImageUrl:
+    "https://firebasestorage.googleapis.com/v0/b/blogskitter-dev.appspot.com/o/cover-images%2Fblog-cover-1698140494229_kafrr.jpg?alt=media&token=d53fb4c7-2f5e-4bed-a9cf-ebabbf7b26f7",
   createdDate: new Date().toISOString(),
   description: "Demo Description " + (index + 1),
   title: "Fake Title " + (index + 1),
