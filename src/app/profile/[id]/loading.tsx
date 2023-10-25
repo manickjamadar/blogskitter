90627c8ae575e02641352d8e08f81389cfdd2a03
Manick <manickware@gmail.com>
2023-10-25 18:56:32 +0530

    profile page loading skeleton used in loading page

--- src/app/profile/[id]/loading.tsx
+++ src/app/profile/[id]/loading.tsx
@@ -1,7 +1,14 @@
+import ProfileBlogListSkeleton from "@/views/profile_blog_list_skeleton/profile_blog_list_skeleton";
+import ProfileSkeleton from "@/views/profile_skeleton/profile_skeleton";
 import React from "react";
 
 const ProfileLoading = () => {
-  return <div>ProfileLoading....</div>;
+  return (
+    <div>
+      <ProfileSkeleton />
+      <ProfileBlogListSkeleton length={10} />
+    </div>
+  );
 };
 
 export default ProfileLoading;
