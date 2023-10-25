2c9ae00d2454dbd430ee19f6bc00db187caee78b
Manick <manickware@gmail.com>
2023-10-25 14:14:03 +0530

    protected the page from unauthorized user

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
+import useAuth from "@/hooks/use_auth";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const ProfilePage = ({ params }: { params: { id: string } }) => {
   const profileId = params.id;
+  const router = useRouter();
+  const { isLoggedIn, user } = useAuth();
+  const isUserAuthorized = isLoggedIn && user && user.id === profileId;
+  useEffect(() => {
+    if (!isUserAuthorized) {
+      router.replace("/");
+    }
+  }, [isUserAuthorized, router]);
+  if (!isUserAuthorized) {
+    return null;
+  }
   return <div>ProfilePage id: {profileId}</div>;
 };
 
