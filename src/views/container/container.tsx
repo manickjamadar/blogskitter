9b93b580f51797395808ce78fde6669afb082a99
Manick <manickware@gmail.com>
2023-10-26 19:31:22 +0530

    initial container component created

---
+++ src/views/container/container.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Container = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="mx-auto w-10/12 max-w-5xl pt-10 pb-10">{children}</div>
+  );
+};
+
+export default Container;
