480abc427a7d8e40224a4658e5b84df5d75ea22b
Manick <manickware@gmail.com>
2023-10-26 19:32:48 +0530

    main article and latest aritcled added in main blog page

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,10 +1,9 @@
 import { apiBlogService } from "@/api_services";
 import ApiError from "@/domain/error/api_error";
 import React from "react";
-import formatDate from "@/utils/format_date";
-import CustomImage from "@/views/custom_image/custom_image";
 import Link from "next/link";
-import DemoBlogContent from "@/views/demo_blog_content/demo_blog_content";
+import LatestArticles from "@/views/latest_articles/latest_article";
+import MainArticle from "@/views/main_article/main_article";
 const BlogPage = async ({ params }: { params: { id: string } }) => {
   const blogId = params.id;
   const blogOrError = await apiBlogService.getBlogById(blogId);
@@ -22,22 +21,10 @@ const BlogPage = async ({ params }: { params: { id: string } }) => {
       </div>
     );
   }
-  const { coverImageUrl, createdDate, description, title } = blogOrError;
   return (
-    <div className="p-10 max-w-2xl mx-auto">
-      <div className="rounded-lg overflow-hidden mb-4">
-        <CustomImage
-          src={coverImageUrl}
-          alt="Blog Image"
-          errorSrc="/images/blog-cover-error-placeholder.png"
-        />
-      </div>
-      <h1 className="text-2xl font-bold mb-2 capitalize">{title}</h1>
-      <p className="text-gray-400 italic text-sm mb-4">
-        Created at {formatDate(new Date(createdDate))}
-      </p>
-      <p className="text-gray-600">{description}</p>
-      <DemoBlogContent />
+    <div>
+      <MainArticle blog={blogOrError} />
+      <LatestArticles />
     </div>
   );
 };
