613330c960c7a12374be69ccd4967dd82fd2f23e
Manick <manickware@gmail.com>
2023-10-26 09:30:03 +0530

    initial blog edit page created

--- src/app/blog/edit/[id]/page.tsx
+++ src/app/blog/edit/[id]/page.tsx
@@ -1,7 +1,19 @@
+import { apiBlogService } from "@/api_services";
+import ApiError from "@/domain/error/api_error";
+import ProtectedPage from "@/views/protected_page/protected_page";
 import React from "react";
 
-const BlogEditPage = () => {
-  return <div>BlogEditPage</div>;
+const BlogEditPage = async ({ params }: { params: { id: string } }) => {
+  const blogId = params.id;
+  const blogOrError = await apiBlogService.getBlogById(blogId);
+  if (blogOrError instanceof ApiError) {
+    return <p className="text center p-4">Blog loading failed</p>;
+  }
+  return (
+    <ProtectedPage>
+      <div>Blog title: {blogOrError.title}</div>
+    </ProtectedPage>
+  );
 };
 
 export default BlogEditPage;
