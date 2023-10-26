0416cd94421a758ca51fb0efd135e0a7a144f869
Manick <manickware@gmail.com>
2023-10-26 19:31:38 +0530

    latest article component created

---
+++ src/views/latest_articles/latest_article.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useAppSelector } from "@/store/store";
+import React from "react";
+import BlogCardList from "../blog_card_list/blog_card_list";
+import { useRouter } from "next/navigation";
+import Container from "../container/container";
+
+const LatestArticles = () => {
+  const router = useRouter();
+  const blogs = useAppSelector((state) => state.blogs.values).slice(0, 6);
+  return (
+    <Container>
+      <h3 className="text-2xl text-center text-gray-700 font-medium mb-6">
+        Latest Articles
+      </h3>
+      <BlogCardList
+        blogs={blogs}
+        onClick={(blog) => {
+          router.push("/blog/" + blog.id);
+        }}
+      />
+    </Container>
+  );
+};
+
+export default LatestArticles;
