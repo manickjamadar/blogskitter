c6442aa8a056b013f66197b29ae7887d65f0d241
Manick <manickware@gmail.com>
2023-10-26 09:28:58 +0530

    iniital blog create form loading page created

---
+++ src/app/blog/create/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogCreatePageLoading = () => {
+  return (
+    <div className="text-xl text-center p-4 font-medium">
+      Blog Create Form Loading
+    </div>
+  );
+};
+
+export default BlogCreatePageLoading;
