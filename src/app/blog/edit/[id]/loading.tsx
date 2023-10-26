ec2c9c12c7590467aefbbd2e7e1ce571c5fd8c47
Manick <manickware@gmail.com>
2023-10-26 09:28:08 +0530

    initial blog edit page loading page added

---
+++ src/app/blog/edit/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogEditPageLoading = () => {
+  return (
+    <div className="text-xl text-center p-4 font-medium">
+      Blog Edit Page Loading
+    </div>
+  );
+};
+
+export default BlogEditPageLoading;
