9e347b5a14411d00122cc10a0da6115db69d88cc
Manick <manickware@gmail.com>
2023-10-26 12:55:12 +0530

    update blog controller added in contoller index file

--- src/controllers/blog_controller/index.ts
+++ src/controllers/blog_controller/index.ts
@@ -1,9 +1,11 @@
 import createBlogController from "./create_blog_controller";
 import deleteBlogController from "./delete_blog_controller";
 import getBlogsController from "./get_blogs_controller";
+import updateBlogController from "./update_blog_controller";
 const BlogController = {
   createBlog: createBlogController,
   getBlogs: getBlogsController,
   deteleBlog: deleteBlogController,
+  updateBlog: updateBlogController,
 };
 export default BlogController;
