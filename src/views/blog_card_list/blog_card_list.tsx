77af0625b2c1e9426ce1fda092a1a26667adc274
Manick <manickware@gmail.com>
2023-10-26 20:09:04 +0530

    vertical props added in blog card list

--- src/views/blog_card_list/blog_card_list.tsx
+++ src/views/blog_card_list/blog_card_list.tsx
@@ -4,13 +4,22 @@ import BlogCard from "../blog_card/blog_card";
 interface Props {
   blogs: IBlogModel[];
   onClick?: (blog: IBlogModel) => void;
+  vertical?: boolean;
 }
-const BlogCardList: React.FC<Props> = ({ blogs, onClick }) => {
+const BlogCardList: React.FC<Props> = ({
+  blogs,
+  onClick,
+  vertical = false,
+}) => {
   if (blogs.length < 1) {
     return null;
   }
   return (
-    <div className="flex gap-6 flex-wrap box-border justify-center">
+    <div
+      className={`flex ${
+        vertical ? "flex-col" : "flex-row"
+      } gap-6 flex-wrap box-border justify-center`}
+    >
       {blogs.map((blog) => (
         <BlogCard
           key={blog.id}
