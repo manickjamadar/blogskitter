324e6d09444e38cb4a854c34d43d7a31106ed670
Manick <manickware@gmail.com>
2023-10-28 14:59:49 +0530

    onclick supported in full blog card list

--- src/views/full_blog_card_list/full_blog_card_list.tsx
+++ src/views/full_blog_card_list/full_blog_card_list.tsx
@@ -3,12 +3,17 @@ import React from "react";
 import FullBlogCard from "../full_blog_card/full_blog_card";
 interface Props {
   blogs: IBlogModel[];
+  onClick?: (blog: IBlogModel) => void;
 }
-const FullBlogCardList: React.FC<Props> = ({ blogs }) => {
+const FullBlogCardList: React.FC<Props> = ({ blogs, onClick }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {blogs.map((blog) => (
-        <FullBlogCard key={blog.id} blog={blog} />
+        <FullBlogCard
+          key={blog.id}
+          blog={blog}
+          onClick={() => onClick && onClick(blog)}
+        />
       ))}
     </div>
   );
