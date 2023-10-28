afc85d657fd9ddbeafd739de8c6916b4d5dada81
Manick <manickware@gmail.com>
2023-10-28 11:57:57 +0530

    demo blog data limit logic added

--- src/domain/data/demo_blog_data.ts
+++ src/domain/data/demo_blog_data.ts
@@ -10,6 +10,15 @@ const demoBlogData: IBlogModel[] = Array.from({ length: 10 }, (_, index) => ({
   uid: "fakeuid",
 }));
 export const getDemoBlogData = (limit: number, skip: number) => {
-  return demoBlogData.slice(skip, skip + limit);
+  return Array.from({ length: limit }, (_, index) => ({
+    id: Math.random().toString(36).substring(2, 7),
+    categories: ["coding"],
+    coverImageUrl:
+      "https://firebasestorage.googleapis.com/v0/b/blogskitter-dev.appspot.com/o/cover-images%2Fblog-cover-1698140494229_kafrr.jpg?alt=media&token=d53fb4c7-2f5e-4bed-a9cf-ebabbf7b26f7",
+    createdDate: new Date().toISOString(),
+    description: "Demo Description " + (index + 1),
+    title: "Fake Title " + (index + 1),
+    uid: "fakeuid",
+  })).slice(skip, skip + limit);
 };
 export default demoBlogData;
