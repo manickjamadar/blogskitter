91f24d3974d36e2c77f71157bb1cc6a198a2b8eb
Manick <manickware@gmail.com>
2023-10-28 06:02:07 +0530

    added proper description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,9 @@ import BlogProvider from "@/views/blog_provider/blog_provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Blogskitter - The Only Coding Blog You need",
-  description: "This is your one stop solution as a developer",
+  title: "Blogskitter - Where readers and writers can leverage their knowledge",
+  description:
+    "Blogskitter is an open platform where readers find dynamic thinking, and where expert and undiscovered voices can share their writing on any topic",
 };
 
 export default function RootLayout({
