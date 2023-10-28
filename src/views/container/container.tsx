5c4c2b342b824d5bbed38b0e3c6c18f4c5c58ebc
Manick <manickware@gmail.com>
2023-10-28 11:58:36 +0530

    container responsiveness fixed

--- src/views/container/container.tsx
+++ src/views/container/container.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto w-10/12 max-w-5xl pt-10 pb-10">{children}</div>
+    // <div className="mx-auto w-10/12 max-w-5xl pt-10 pb-10">{children}</div>
+    <div className="px-8 max-w-7xl mx-auto">{children}</div>
   );
 };
 
