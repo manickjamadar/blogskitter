d53c50f576cff1cf1e1c4fc046a39392be92f58a
Manick <manickware@gmail.com>
2023-10-28 15:17:08 +0530

    home page is redesigned with full blog card , side blog card and blog card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 import config from "@/domain/config";
+import { getDemoBlogData } from "@/domain/data/demo_blog_data";
 import BlogsSelector from "@/store/selectors/blogs_selector";
 import { BlogsActions } from "@/store/slices/blogs_slice";
 import { useAppDispatch, useAppSelector } from "@/store/store";
+import formatDate from "@/utils/format_date";
+import truncateString from "@/utils/truncate_string";
 import BlogCardList from "@/views/blog_card_list/blog_card_list";
 import BlogCardSkeletonList from "@/views/blog_card_skeleton_list/blog_card_skeleton_list";
 import Container from "@/views/container/container";
+import Hero from "@/views/hero/hero";
+import FullBlogCard from "@/views/full_blog_card/full_blog_card";
 import { useRouter } from "next/navigation";
+import FullBlogCardList from "@/views/full_blog_card_list/full_blog_card_list";
+import SideBlogCardList from "@/views/side_blog_card_list/side_blog_card_list";
+import { IBlogModel } from "@/domain/models/blog";
 
 export default function HomePage() {
   const dispatch = useAppDispatch();
@@ -27,6 +35,9 @@ export default function HomePage() {
       })
     );
   };
+  const handleBlogClick = (blog: IBlogModel) => {
+    router.push(`/blog/${blog.id}`);
+  };
   const blogs = useAppSelector((state) => state.blogs.values);
   if (isFetchingFailed) {
     return (
@@ -37,13 +48,55 @@ export default function HomePage() {
       </p>
     );
   }
+  const demoBlogs = getDemoBlogData(24, 0);
+  const topBlog = blogs[0];
+  const sideBlogs = blogs.slice(1, 6);
+  const featuredArticles = blogs.slice(6, 12);
+  const latestArticles = blogs.slice(12);
   return (
     <Container>
-      {blogs.length > 0 && (
-        <BlogCardList
-          blogs={blogs}
-          onClick={(blog) => router.push("/blog/" + blog.id)}
-        />
+      <Hero />
+      <div className="flex gap-6">
+        {topBlog && sideBlogs.length < 1 && (
+          <FullBlogCard
+            blog={topBlog}
+            showDetails
+            onClick={() => handleBlogClick(topBlog)}
+          />
+        )}
+        {topBlog && sideBlogs.length > 0 && (
+          <>
+            <div className="blog-gray-100 w-full lg:w-2/3">
+              <FullBlogCard
+                blog={topBlog}
+                showDetails
+                onClick={() => handleBlogClick(topBlog)}
+              />
+            </div>
+            <div className="hidden lg:block blog-gray-100 w-1/3">
+              <SideBlogCardList blogs={sideBlogs} onClick={handleBlogClick} />
+            </div>
+          </>
+        )}
+      </div>
+      {featuredArticles.length > 0 && (
+        <div className="py-8 border-b">
+          <h2 className="font-medium text-2xl text-center sm:text-left mb-6">
+            Featured Articles
+          </h2>
+          <FullBlogCardList
+            blogs={featuredArticles}
+            onClick={handleBlogClick}
+          />
+        </div>
+      )}
+      {latestArticles.length > 0 && (
+        <div className="py-8" id="latest-articles">
+          <h2 className="font-medium text-2xl text-center sm:text-left mb-6">
+            Latest Articles
+          </h2>
+          <BlogCardList blogs={latestArticles} onClick={handleBlogClick} />
+        </div>
       )}
       {isBlogsFetching && (
         <BlogCardSkeletonList length={config.blog.fetchingLimit} />
