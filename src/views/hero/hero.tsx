01b8aa3cac731d3a0dc4d3b3598c89adbfdd2214
Manick <manickware@gmail.com>
2023-10-28 09:37:56 +0530

    initial hero component created

---
+++ src/views/hero/hero.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+const Hero = () => {
+  return (
+    <div className="flex flex-col gap-6 items-center py-10 border-b">
+      <h1 className="text-center text-3xl sm:text-4xl font-bold max-w-4xl">
+        Uncover fresh perspective, ideas and knowledge through the power of
+        blogs
+      </h1>
+      <p className="text-center max-w-2xl text-sm sm:text-base">
+        Blogskitter is an open platform where readers find dynamic thinking and
+        where expert and undiscovered voices can share their writing on any
+        topic
+      </p>
+      <div className="flex justify-center items-center">
+        <button className="primaryButton">Start Reading</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
