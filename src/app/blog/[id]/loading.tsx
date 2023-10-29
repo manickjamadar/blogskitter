9dff85058a2c6c12f8c5ee30f7a0cee5c7f78597
Manick <manickware@gmail.com>
2023-10-29 14:05:15 +0530

    extra skeleton added in blog loading page

--- src/app/blog/[id]/loading.tsx
+++ src/app/blog/[id]/loading.tsx
@@ -5,12 +5,32 @@ import React from "react";
 const BlogLoading = () => {
   return (
     <Container>
-      <div className="flex gap-6 mt-20 animate-pulse">
+      <div className="flex gap-6 mt-20 mb-20 animate-pulse">
         <div className="w-full lg:w-2/3">
           <p className="mb-4 text-gray-500 text-sm italic w-1/2 h-3 rounded bg-gray-100"></p>
           <p className="text-gray-400 italic text-sm mb-2 w-1/4 h-3 rounded bg-gray-100"></p>
           <h1 className="text-2xl font-bold mb-4 capitalize w-10/12 h-4 rounded bg-gray-200"></h1>
           <div className="rounded-lg overflow-hidden mb-4 bg-gray-100 w-full aspect-video"></div>
+          <div className="flex flex-col gap-3">
+            <p className="w-1/2 h-6 bg-gray-100 rounded"></p>
+            <p className="w-8/12 h-3 bg-gray-100 rounded"></p>
+            <p className="w-7/2 h-3 bg-gray-100 rounded"></p>
+            <p className="w-6/12 h-2 bg-gray-100 rounded"></p>
+            <p className="w-full h-10 bg-gray-100 rounded"></p>
+            <p className="w-1/3 h-2 bg-gray-100 rounded"></p>
+            <p className="w-1/3 h-2 bg-gray-100 rounded"></p>
+            <p className="w-8/12 h-2 bg-gray-100 rounded"></p>
+            <p className="w-7/12 h-3 bg-gray-100 rounded"></p>
+            <p className="w-1/2 h-4 bg-gray-100 rounded"></p>
+            <p className="w-8/12 h-3 bg-gray-100 rounded"></p>
+            <p className="w-7/2 h-3 bg-gray-100 rounded"></p>
+            <p className="w-6/12 h-2 bg-gray-100 rounded"></p>
+            <p className="w-full h-10 bg-gray-100 rounded"></p>
+            <p className="w-1/3 h-2 bg-gray-100 rounded"></p>
+            <p className="w-1/3 h-2 bg-gray-100 rounded"></p>
+            <p className="w-8/12 h-2 bg-gray-100 rounded"></p>
+            <p className="w-7/12 h-3 bg-gray-100 rounded"></p>
+          </div>
         </div>
         <div className="pt-12 hidden lg:block w-1/3">
           <div className="">
